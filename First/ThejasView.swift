32750cc73002a8d1dd462db53a7961bea398387b
Thejas775 <thejaselandassery@gmail.com>
2024-12-06 03:07:19 +0530

    Added a button to navigate to second page

--- First/ThejasView.swift
+++ First/ThejasView.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct ThejasView: View {
     var body: some View {
-        Text("Hello, All! This app is made by Thejas Elandassery. This is my first app so please bear with me. It's a simple navigation from splash screen to home screen after 2 seconds.")
-            .navigationBarBackButtonHidden(true)  // This hides the back button
-
+        NavigationStack {
+            VStack {
+                Text("Hello, All! This app is made by Thejas Elandassery. This is my first app so please bear with me. It's a simple navigation from splash screen to home screen after 2 seconds.")
+                    .padding()
+                
+                NavigationLink(destination: DetailView()) {
+                    Text("Go to Next Screen")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            .navigationBarBackButtonHidden(true)
+        }
     }
 }
 
